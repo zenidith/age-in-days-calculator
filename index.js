8a9799f1b38af0ba097e6ce87c2138fc9b4cbcb7
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-04-25 12:51:08 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var currentTime = Date.now();
+
+let dob = "1982/02/17";
+
+let timeDifference = currentTime - new Date(dob);
+
+console.log(new Date(dob));
+console.log(currentTime);
+console.log(timeDifference);
